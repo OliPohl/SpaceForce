89b9ec7fa13db1247f1ab46b3992150e2ef17aff
OliverPohl <oliver@pohl66.de>
2022-12-14 08:02:48 +0100

    Bounds working

--- SpaceForcing/Assets/_Scripts/Player/BoundariesScript.cs
+++ SpaceForcing/Assets/_Scripts/Player/BoundariesScript.cs
@@ -1,7 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*  
+    ####################################################
+    #                   Boundaries.cs                  #
+    #--------------------------------------------------#
+    # This script makes stops the player from escaping #
+    # the camera.                                      #
+    ####################################################
+*/  
 public class BoundariesScript : MonoBehaviour
 {
     private Vector2 ScreenBounds;
@@ -12,18 +19,18 @@ public class BoundariesScript : MonoBehaviour
 
     void Start()
     {
-        ScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        ScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));  // Getting the screen width and height with camera units.
 
-        ObjectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
-        ObjectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        ObjectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;   // Gives half of the spaceships width.
+        ObjectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;  // Gives half of the spaceships height.
     }
 
     
     void LateUpdate()
     {
-        Vector3 ViewPos = transform.position;
-        ViewPos.x = Mathf.Clamp(ViewPos.x, ((ScreenBounds.x + ObjectWidth) * -1), (ScreenBounds.x + ObjectWidth));
-        ViewPos.y = Mathf.Clamp(ViewPos.y, ((ScreenBounds.y + ObjectHeight) * -1), (ScreenBounds.y + ObjectHeight));
-        transform.position = ViewPos;
+        Vector3 ViewPos = transform.position; 
+        ViewPos.x = Mathf.Clamp(ViewPos.x, ((ScreenBounds.x - ObjectWidth) * -1), (ScreenBounds.x - ObjectWidth));      // Gives back ViewPos.x if it is inside of the screen - Spaceship width.
+        ViewPos.y = Mathf.Clamp(ViewPos.y, ((ScreenBounds.y - ObjectHeight) * -1), (ScreenBounds.y - ObjectHeight));    // Gives back ViewPos.y if it is inside of the screen - Spaceship height.
+        transform.position = ViewPos;   // Updates the Position. Honestly I have no idea why this shit is working. FrFr.
     }
 }
